4525477731edb150ff94ad47a9785ad0b9a07180
RAStoychev18 <RAStoychev18@codingburgas.bg>
2022-11-14 13:57:19 +0200

    adding files

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Users(
+                 ID INTEGER NOT NULL,
+                 userName VARCHAR(255) NOT NULL,
+                 Password VARCHAR(255) NOT NULL,
+                 personalName VARCHAR(255) NOT NULL,
+                 familyName VARCHAR(255) NOT NULL,
+                 role VARCHAR(255) NOT NULL,
+                 team VARCHAR(255) NOT NULL,
+                 PRIMARY KEY (ID)
+);
